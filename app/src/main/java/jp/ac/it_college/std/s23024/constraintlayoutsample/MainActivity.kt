a1163495a0bea463a20f32605b3d9d6cadd0dca8
itc-s23024 <s23024@std.it-college.ac.jp>
2024-07-31 16:21:42 +0900

    chap6

---
+++ app/src/main/java/jp/ac/it_college/std/s23024/constraintlayoutsample/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.ac.it_college.std.s23024.constraintlayoutsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23024.constraintlayoutsample.databinding.ActivitySecondBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
